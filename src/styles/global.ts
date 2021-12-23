6acd4b1f6dc172f321a5acb15546212f5b68487e
Victor Freitas <victorprogr@gmail.com>
2021-12-23 02:02:31 -0300

    add variable colors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,22 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+
+  :root {
+    --background: #170808;
+    --white: #FFFFFC;
+    --pink: #CF8BA9;
+    --red: #942020;
+
+    --container: 100rem;
+
+    --small: 2rem;
+    --medium: 3rem;
+    --large: 5rem;
+    --huge: 7rem;
+
+    }
+
   * {
     margin: 0;
     padding: 0;
@@ -12,10 +28,16 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   html, body, #__next {
-    height: 100%
+    height: 100%;
+    background: var(--background);
+    color:  var(--white);
   }
 
   body {
     font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p {
+    font-size: 1.1rem;
   }
 `
